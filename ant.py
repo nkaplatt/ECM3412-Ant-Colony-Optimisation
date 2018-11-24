0958a4cc16cf91f14c36d30c2318a6961d6ff76e
nickplatt <np326@exeter.ac.uk>
2018-11-24 09:31:36 +0000

    bin selection

--- ant.py
+++ ant.py
@@ -4,6 +4,7 @@ def generate_ant_paths(number_of_ants, items, graph):
   paths = []
   for ant in range(number_of_ants):
     paths.append([choose_bin(index, graph) for index, item in enumerate(items)])
+  print('paths:', paths)
   return paths
 
 def choose_bin(item_number, graph):
@@ -20,7 +21,7 @@ def choose_bin(item_number, graph):
   total_pheromone = sum(graph.get(item_number))
   weights = [(path_pheromone / total_pheromone) for path_pheromone in graph.get(item_number)]
   path_choices = range(len(graph.get(item_number)))
-  
+  print('path choices:', path_choices, 'path weights:', weights)
   chosen_bin = random.choices(path_choices, weights) # randomly select a bin (path) to take, returns as a list
   return chosen_bin[0] # to just return the int
 
@@ -58,4 +59,6 @@ def evaluate_fitness(path, items, bins):
   bins = [0]*bins # list of bins with weight initialised at 0
   for path_choice, item in zip(path, items): # iterate through items and add to chosen bin
     bins[path_choice] += item
+  if (max(bins) - min(bins)) == 0: # dividing by 0 causes error, catch
+    return 100/ 0.0000000001
   return 100/ (max(bins) - min(bins))
--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import ant
 def bin_packing(number_bins, number_items, evaporation_rate, ants):
   # initialise bins, items weights and evaluations
   item_weights = init.generate_item_weights(number_bins, number_items)
-  fitness_evaluations = 5
-
+  fitness_evaluations = 100
   problem_graph = init.generate_construction_graph(number_bins, number_items)
-  
+  best_fitness = 0
+
   i = 0
   while i < fitness_evaluations:
-    print('new run:', i)
-    ant_optimisation(ants, item_weights, number_bins, problem_graph, evaporation_rate)
+    optimised_fitness = ant_optimisation(ants, item_weights, number_bins, problem_graph, evaporation_rate, best_fitness)
+    # print('i:', i, 'best:', best_fitness, 'optimised:', optimised_fitness)
+    if best_fitness < optimised_fitness:
+      best_fitness = optimised_fitness
     i += 1
+  return best_fitness
 
-def ant_optimisation(ants, items, bins, problem_graph, evaporation_rate):
+def ant_optimisation(ants, items, bins, problem_graph, evaporation_rate, best_fitness):
   '''[summary]
   
   Arguments:
@@ -25,12 +28,16 @@ def ant_optimisation(ants, items, bins, problem_graph, evaporation_rate):
     problem_graph {dictionary} -- construction graph of problem space
   '''
   ant_paths = ant.generate_ant_paths(ants, items, problem_graph)
+
   for path in ant_paths:
-    fitness_of_path = [ant.evaluate_fitness(path, items, bins)]
+    fitness_of_path = ant.evaluate_fitness(path, items, bins)
     problem_graph = ant.update_pheromone_fitness(path, fitness_of_path, problem_graph)
     problem_graph = ant.evaporate_pheromone(problem_graph, evaporation_rate)
-    # print(problem_graph)
+    if best_fitness < fitness_of_path:
+      best_fitness = fitness_of_path
+  return best_fitness
 
 
 if __name__ == '__main__':
-  bin_packing(3, 6, 0.9, 10)
+  result = bin_packing(3, 10, 0.9, 10)
+  print(result)
